4a868c0b8d145773d801b8b8544ce9d207366b22
Rodrigo Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2024-01-22 22:22:49 -0300

    Update disciplinas-do-curriculo.js
    Formatação padrão

--- disciplinas-do-curriculo.js
+++ disciplinas-do-curriculo.js
@@ -1,16 +1,12 @@
-function envia( codigo )
-     {
-              criaInputHidden('disciplinas[0]', codigo);
-              document.output.submit();
-     }
+function envia(codigo) {
+    criaInputHidden('disciplinas[0]', codigo);
+    document.output.submit();
+}
 
-     function criaInputHidden(campo, valor)
-     {
-         var hiddenField = document.createElement('input');            
-         hiddenField.type = 'hidden';
-         hiddenField.name = campo;
-         hiddenField.value = valor;            
-         document.output.appendChild(hiddenField);            
-     }
-
-     
+function criaInputHidden(campo, valor) {
+    var hiddenField = document.createElement('input');
+    hiddenField.type = 'hidden';
+    hiddenField.name = campo;
+    hiddenField.value = valor;
+    document.output.appendChild(hiddenField);
+}
