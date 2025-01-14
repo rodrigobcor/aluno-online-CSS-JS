1cfd6c57d51c82c749ae469574c135aa914f8284
Rodrigo Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2025-01-14 18:41:30 -0300

    Update alunoOnline.js

--- cssJs/alunoOnline.js
+++ cssJs/alunoOnline.js
@@ -37,3 +37,76 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+/*
+ * Bloqueador de Botão Direito - iceBreaker http://www.icebreaker.com.br/ 
+ */
+function clique() 
+{
+    if (event.button==2||event.button==3) 
+    {
+        oncontextmenu = "return false;";
+        alert("Não retorne ou feche a página caso tenha submetido suas alterações.  A resposta do sistema pode demorar em função da concorrência de usuários.");
+    }
+} 
+
+document.onmousedown=clique ;
+document.oncontextmenu = new Function("return false;") ;
+
+
+//function enviarRequisicao(formOutput)
+//{ 
+//    var requisicaoOriginal = formOutput.elements["requisicao"].value;
+//
+//    formOutput.elements["requisicao"].value = requisicao;
+//    formOutput.submit();
+//    formOutput.elements["requisicao"].value = requisicaoOriginal;
+//}
+
+/*
+ *  Consultar Discplina pelo Código.
+ *  
+ *  formOutput: nome do formulário
+ *  codigo: código da disciplina
+ */
+function consultarDisciplina( formOutput, codigo )
+{
+    formCreateHiddenInputField(formOutput, 'disciplinas[0]', codigo);
+    formOutput.submit();
+}
+
+function consultarTurmasDisciplina( formOutput, codigo, requisicao )
+{
+    formCreateHiddenInputField(formOutput, 'disciplinas[0]', codigo);
+
+    var requisicaoOriginal = formOutput.elements["requisicao"].value;
+
+    formOutput.elements["requisicao"].value = requisicao;
+    formOutput.submit();
+    formOutput.elements["requisicao"].value = requisicaoOriginal;
+}
+
+/*
+ *  Cria um campo de formulário input dinamicamente.
+ *  
+ *  formOutput: nome do formulário
+ *  campo: nome do campo a ser criado
+ *  valor: valor do campo a ser criado
+ *  
+ */
+function formCreateHiddenInputField(formOutput, campo, valor)
+{
+    var hiddenField = document.createElement('input');            
+    hiddenField.type = 'hidden';
+    hiddenField.name = campo;
+    hiddenField.value = valor;            
+    formOutput.appendChild(hiddenField);            
+}
+
+
+function ementaDisciplina( idDisciplina )
+{
+    formCreateHiddenInputField(output, 'idDisciplina', idDisciplina);
+    document.output.submit();
+}
