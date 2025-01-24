9eea384bb0549e9de002075b78224b9d988e8d2c
Rodrigo Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2025-01-24 11:22:05 -0300

    Update alunoOnline.js

--- cssJs/alunoOnline.js
+++ cssJs/alunoOnline.js
@@ -1,14 +1,57 @@
-function envia(codigo) {
-    criaInputHidden('disciplinas[0]', codigo);
-    document.output.submit();
+//function enviarRequisicao(formOutput)
+//{ 
+//    var requisicaoOriginal = formOutput.elements["requisicao"].value;
+//
+//    formOutput.elements["requisicao"].value = requisicao;
+//    formOutput.submit();
+//    formOutput.elements["requisicao"].value = requisicaoOriginal;
+//}
+
+/*
+ *  Consultar Discplina pelo Código.
+ *  
+ *  formOutput: nome do formulário
+ *  codigo: código da disciplina
+ */
+function consultarDisciplina( formOutput, codigo )
+{
+    formCreateHiddenInputField(formOutput, 'disciplinas[0]', codigo);
+    formOutput.submit();
 }
 
-function criaInputHidden(campo, valor) {
-    var hiddenField = document.createElement('input');
+function consultarTurmasDisciplina( formOutput, codigo, requisicao )
+{
+    formCreateHiddenInputField(formOutput, 'disciplinas[0]', codigo);
+
+    var requisicaoOriginal = formOutput.elements["requisicao"].value;
+
+    formOutput.elements["requisicao"].value = requisicao;
+    formOutput.submit();
+    formOutput.elements["requisicao"].value = requisicaoOriginal;
+}
+
+/*
+ *  Cria um campo de formulário input dinamicamente.
+ *  
+ *  formOutput: nome do formulário
+ *  campo: nome do campo a ser criado
+ *  valor: valor do campo a ser criado
+ *  
+ */
+function formCreateHiddenInputField(formOutput, campo, valor)
+{
+    var hiddenField = document.createElement('input');            
     hiddenField.type = 'hidden';
     hiddenField.name = campo;
-    hiddenField.value = valor;
-    document.output.appendChild(hiddenField);
+    hiddenField.value = valor;            
+    formOutput.appendChild(hiddenField);            
+}
+
+
+function ementaDisciplina( idDisciplina )
+{
+    formCreateHiddenInputField(output, 'idDisciplina', idDisciplina);
+    document.output.submit();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
