9a40db291737eac164d094fbe37b055b194d6306
aasditeana <aasdteaa@gmail.com>
2025-03-02 00:45:54 -0300

    Removendo "panel"s do acordeão a substituir

--- cssJs/NEWalunoOnline.js
+++ cssJs/NEWalunoOnline.js
@@ -53,30 +53,3 @@ function ementaDisciplina( idDisciplina )
     formCreateHiddenInputField(output, 'idDisciplina', idDisciplina);
     document.output.submit();
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var celulasAcordeao = document.querySelectorAll('.detalhes-disciplina.acordeao');
-
-    celulasAcordeao.forEach(function(celula) {
-        celula.addEventListener('click', function(event) {
-            if (event.target === celula || event.target.classList.contains('texto-nome-disciplina')) {
-                var panel = celula.querySelector('.panel');
-
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                    celula.classList.remove('active');
-                } else {
-                    panel.style.display = "block";
-                    celula.classList.add('active');
-                }
-            }
-        });
-
-        var panel = celula.querySelector('.panel');
-        if (panel) {
-            panel.addEventListener('click', function(event) {
-                event.stopPropagation(); 
-            });
-        }
-    });
-});
