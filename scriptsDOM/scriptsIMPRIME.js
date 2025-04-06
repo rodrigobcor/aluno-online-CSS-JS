f8a3682f79e91108e59c62543717bd0ed2026b92
Rodrigo Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2025-04-06 16:19:27 -0300

    Update scriptsIMPRIME.js
    Removida a função getDisciplinasDaPagina()

--- scriptsDOM/scriptsIMPRIME.js
+++ scriptsDOM/scriptsIMPRIME.js
@@ -1,42 +1,4 @@
 /**
- * Obtém a lista das disciplinas.
- * 
- * Atenção: o conteúdo está hard-coded.
- * Mudanças na estrutura da página impedirão o funcionamento correto.
- * 
- * @todo corrigir valores hard-coded
- */
-function getDisciplinasDaPagina() {
-  const tds = document.querySelectorAll('td');
-  disciplinas = [];
-  // FIXME valores hard-coded
-  for (let i = 19; i <= tds.length-5; i++) {
-    disciplina = {};
-    disciplina.codigoEnome = tds[i].innerText;
-    i++;
-    disciplina.periodo = tds[i].innerText;
-    i++;
-    disciplina.atendida = tds[i].innerText;
-    i++;
-    disciplina.tipo = tds[i].innerText;
-    i++;
-    disciplina.ramif = tds[i].innerText;
-    i++;
-    disciplina.creditos = tds[i].innerText;
-    i++;
-    disciplina.CHtotal = tds[i].innerText;
-    i++;
-    disciplina.travaCredito = tds[i].innerText;
-    i++;
-    disciplina.turmaPeriodo = tds[i].innerText;
-    disciplinas.push(disciplina);
-    i+9;
-  }
-  return disciplinas;
-}
-
-
-/**
  * Obtém os detalhes de determinada disciplina do período, no formato HTML.
  * 
  * Essa função não altera nenhum dado do servidor original.
